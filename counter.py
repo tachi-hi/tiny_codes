3ccc49ff8c90069ebcc1cf5145e53d254dbd4da9
Hideyuki Tachibana <tachi-hi@users.noreply.github.com>
2015-08-08 23:12:47 +0900

    python timer

--- counter.py
+++ counter.py
@@ -1,7 +1,9 @@
 #!/bin/python
 
 from sys import stdout
+import time
 
 for i in range(1,100):
-  stdout.write("\r There are " + str(i) + "apples")
+  stdout.write("\r There are " + str(i) + " apples")
   stdout.flush()
+  time.sleep(0.1)
